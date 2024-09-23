32cc2e932b98a09a4edefe1110488020000724c7
Birdcar <434063+birdcar@users.noreply.github.com>
2024-09-23 15:05:24 -0600

    Test command

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,21 @@
-import { helpers, termost } from "termost";
+import {Command, Option, Cli} from 'clipanion';
 
-type ProgramContext = {
-	globalFlag: string;
-};
+export class HelloCommand extends Command {
+  name = Option.String();
 
-type DebugCommandContext = {
-	localFlag: string;
-};
+  async execute() {
+    this.context.stdout.write(`Hello ${this.name}!\n`);
+  }
+}
 
-const program = termost<ProgramContext>("CLI example", {
-	onException(error) {
-		console.error(`Error logic ${error.message}`);
-	},
-	onShutdown() {
-		console.log("Clean-up logic");
-	},
-});
+const [node, app, ...args] = process.argv;
 
-program.option({
-	key: "globalFlag",
-	name: { long: "global", short: "g" },
-	description:
-		"A global flag/option example accessible by all commands (key is used to persist the value into the context object)",
-	defaultValue:
-		"A default value can be set if no flag is provided by the user",
-});
+const cli = new Cli({
+    binaryLabel: `My Application`,
+    binaryName: `${node} ${app}`,
+    binaryVersion: `1.0.0`,
+})
 
-program
-	.command({
-		name: "build",
-		description:
-			"A custom command example runnable via `bin-name build` (command help available via `bin-name build --help`)",
-	})
-	.task({
-		label: "A label can be displayed to follow the task progress",
-		async handler() {
-			await fakeBuild();
-		},
-	});
+cli.register(HelloCommand);
+cli.runExit(args);
 
-program
-	.command<DebugCommandContext>({
-		name: "debug",
-		description: "A command to play with Termost capabilities",
-	})
-	.option({
-		key: "localFlag",
-		name: "local",
-		description: "A local flag accessible only by the `debug` command",
-		defaultValue: "local-value",
-	})
-	.task({
-		handler(context, argv) {
-			helpers.message(`Hello, I'm the ${argv.command} command`);
-			helpers.message(`Context value = ${JSON.stringify(context)}`);
-			helpers.message(`Argv value = ${JSON.stringify(argv)}`);
-		},
-	});
-
-const fakeBuild = async () => {
-	return new Promise((resolve) => {
-		setTimeout(resolve, 3000);
-	});
-};
